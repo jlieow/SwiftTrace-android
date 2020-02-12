808f695864e28db87c65626b25ec19810a08388d
Jerome Lieow <jeromelieow@gmail.com>
2020-02-12 19:36:52 +0800

    Fixed fatal error: Cannot create PhoneAuthCredential without either verificationProof, sessionInfo, or temprary proof
    Fixed fatal error: Cannot create PhoneAuthCredential without either verificationProof, sessionInfo, or temprary proof by removing intent setFlags function.

--- app/src/main/java/com/swiftoffice/swifttrace/OTP.java
+++ app/src/main/java/com/swiftoffice/swifttrace/OTP.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -69,7 +70,6 @@ public class OTP extends AppCompatActivity {
 
     void openHomePage() {   //Launches Home Page
         Intent intent = new Intent(this, HomePage.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
 
