c7015962aeb6e0d0f2cd2e7d717a4d788bdc5fee
Jerome Lieow <jeromelieow@gmail.com>
2020-02-20 00:34:16 +0800

    Inserted Firebase Cloud Messaging Code
    1. Retrieves Firebase Cloud Messaging ID
    2. Implemented new class to listen for notification and data messages

--- app/src/main/java/com/swiftoffice/swifttrace/activities/HomeActivity.java
+++ app/src/main/java/com/swiftoffice/swifttrace/activities/HomeActivity.java
@@ -1,17 +1,29 @@
 package com.swiftoffice.swifttrace.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 import com.swiftoffice.swifttrace.R;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -22,12 +34,19 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     private Toolbar toolBar;
     private TextView tvToolBarTitle;
 
+    String firebaseid;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
         initViews();
+
+
+
+        //Retrieves for Firebase Messaging ID
+        RetrieveFirebaseID();
 
         //Set toolbar title
         tvToolBarTitle.setText(getResources().getString(R.string.swift_trace));
@@ -84,6 +103,47 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
+    public void RetrieveFirebaseID() {
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w("Fail", "getInstanceId failed", task.getException());
+                        }
+
+                        // Get new Instance ID token
+                        firebaseid = task.getResult().getToken();
+
+                        // Log and toast
+                        String msg = getString(R.string.msg_token_fmt, firebaseid);
+                        Log.d("Success", msg);
+                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+
+                        try {   //Creates and stores Firebase Messaging ID in a app specific cache storage
+                            String filename = getResources().getString(R.string.firebase_messaging_id);
+                            File.createTempFile(filename, null, getApplicationContext().getCacheDir());
+                            File cacheFile = new File(getApplicationContext().getCacheDir(), filename);
+
+                            FileWriter fileWriter = new FileWriter(cacheFile.getAbsoluteFile());
+                            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                            bufferedWriter.write(firebaseid);
+                            bufferedWriter.close();
+
+                            FileReader fileReader = new FileReader(cacheFile.getAbsoluteFile());
+                            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+                            Log.w("Success", getResources().getString(R.string.successfully_written_to_cache, bufferedReader.readLine()));
+
+                        } catch (IOException e) {
+                            Log.w("Fail", getResources().getString(R.string.failed_to_write_to_cache));
+                            e.printStackTrace();
+                        }
+                    }
+                });
+    }
+
+
     //Redirect to health declaration screen
     void openHealthDeclaration() {
         Intent intent = new Intent(this, HealthDeclarationActivity.class);
@@ -92,12 +152,14 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+
     //Redirect to Temperature record screen
     void openTemperatureRecord() {
         Intent intent = new Intent(this, TemperatureRecordActivity.class);
         startActivity(intent);
         overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
     }
+
 
     //IN Progress
     void openEnableTracing() {
